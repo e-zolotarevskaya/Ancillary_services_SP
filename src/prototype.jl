93aad00c902dcea98cd224bcde8eebfd304f1c00
Ekaterina Zolotarevskaia <zkatya123@gmail.com>
2022-01-20 14:39:42 +0100

    Set upper bounds for investment, select day in dat

--- src/prototype.jl
+++ src/prototype.jl
@@ -46,50 +46,51 @@ end
 # Global system parameters
 c_i = .03
 c_o = .01
-t_f = 48
 t_s = 1
+t_f = 48
+offset = 2400
 timesteps = t_s:t_f
 time_scale = 10. *365*24/length(timesteps)
-c_pv = 1000.
+c_pv = 100.
 c_wind = 1000.
-c_storage = 400.
+c_storage = 100.
 c_flex = .5
-F = 100.
+F = 10.
 storage_scale = 1.
 ##
 # Define weather and demand data
-pv = CSV.read("../data/pv_Halle18.csv", DataFrame)[timesteps, 1]
+#=pv = CSV.read("../data/pv_Halle18.csv", DataFrame)[timesteps, 1]
 wind = CSV.read("../data/wind_Karholz.csv", DataFrame)[timesteps, 1]
-demand = CSV.read("../data/demand_Industriepark.csv", DataFrame)[timesteps, 1]
-#=pv = CSV.read("../data/basic_example.csv", DataFrame)[timesteps, 3]
-wind = CSV.read("../data/basic_example.csv", DataFrame)[timesteps, 4]
-demand = CSV.read("../data/basic_example.csv", DataFrame)[timesteps, 2]
-=#
+demand = CSV.read("../data/demand_Industriepark.csv", DataFrame)[timesteps, 1]=#
+pv = CSV.read("../data/basic_example.csv", DataFrame)[timesteps.+offset, 3]
+wind = CSV.read("../data/basic_example.csv", DataFrame)[timesteps.+offset, 4]
+demand = CSV.read("../data/basic_example.csv", DataFrame)[timesteps.+offset, 2]
+
 ##
 function scenarios(n, timesteps)
-    return [@scenario t_xi = rand(1:length(timesteps)-2) s_xi = rand([-1, 1]) probability = 1. /n for i in 1:n]
+    return [@scenario t_xi = rand(timesteps[1:end-2]) s_xi = rand([-1, 1]) probability = 1. /n for i in 1:n]
 end
 ##
 # Note on signs: s_xi<0 means that energy is requested, s_xi>0 means an additional consumption
 @stochastic_model em begin
     @stage 1 begin
         # Investments:
-        @decision(em, u_pv >= 0)
-        @decision(em, u_wind >= 0)
-        @decision(em, u_storage >= 0)
+        @decision(em, 100000 >= u_pv >= 0)
+        @decision(em, 100000 >= u_wind >= 0)
+        @decision(em, 100000 >= u_storage >= 0)
         # Grid connection
         @decision(em, gci[t in timesteps] >= 0)
         @decision(em, gco[t in timesteps] >= 0)
         # Storage model
         @decision(em, storage[t in timesteps])
-        @constraint(em, [t in timesteps], -0.5*u_storage*storage_scale <= sum(storage[1:t]))
-        @constraint(em, [t in timesteps], sum(storage[1:t]) <= 0.5*u_storage*storage_scale)
+        @constraint(em, [t in timesteps], -0.5*u_storage*storage_scale <= sum(storage[t_s:t]))
+        @constraint(em, [t in timesteps], sum(storage[t_s:t]) <= 0.5*u_storage*storage_scale)
         @constraint(em, sum(storage) == 0)
         # Energy balance
         @constraint(em, [t in timesteps], 
         gci[t]-gco[t]+u_pv*pv[t]+u_wind*wind[t]-demand[t]+storage[t]==0) # Energy balance
         # Investment costs
-        @objective(em, Min, u_pv*c_pv/time_scale+u_wind*c_wind/time_scale+u_storage*c_storage/time_scale + c_i*sum(gci) - c_o*sum(gco))
+        @objective(em, Min, u_pv*c_pv/time_scale+u_wind*c_wind/time_scale+u_storage*c_storage/time_scale+c_i*sum(gci)-c_o*sum(gco))
     end
     @stage 2 begin
         @uncertain t_xi s_xi #t_xi the time of flexibility demand, s_xi - sign (±1 or 0)
@@ -103,8 +104,8 @@ end
         @recourse(em, gci2[t in t_xi+1:t_f] >= 0)
         @recourse(em, gco2[t in t_xi+1:t_f] >= 0)
         @recourse(em, sto2[t in timesteps])
-        @constraint(em, [t in timesteps], -0.5*u_storage*storage_scale <= sum(sto2[1:t]))
-        @constraint(em, [t in timesteps], sum(sto2[1:t]) <= 0.5*u_storage*storage_scale)
+        @constraint(em, [t in timesteps], -0.5*u_storage*storage_scale <= sum(sto2[t_s:t]))
+        @constraint(em, [t in timesteps], sum(sto2[t_s:t]) <= 0.5*u_storage*storage_scale)
         @constraint(em, sum(sto2) == 0)
 
         # Put storage at time of event into same state
@@ -124,13 +125,13 @@ end
         # Post event energy balance
         @constraint(em, [t in (t_xi+1):t_f],
         gci2[t]-gco2[t]+u_pv*pv[t]+u_wind*wind[t]-demand[t]+sto2[t]==0)
-        @objective(em, Min, c_i*sum(gci2[t_xi+1:t_f])-c_o*sum(gco2[t_xi+1:t_f]) )#- (c_i*sum(gci[t_xi+1:t_f])-c_o*sum(gco[t_xi+1:t_f])) )
+        @objective(em, Min, c_i*sum(gci2[t_xi+1:t_f])-c_o*sum(gco2[t_xi+1:t_f]) - (c_i*sum(gci[t_xi+1:t_f])-c_o*sum(gco[t_xi+1:t_f])))
     end
 end
 ## 
 #xi_1 = @scenario t_xi = 5 s_xi = -1 probability = 0.5
-#xi_2 = @scenario t_xi = 3 s_xi = -1 probability = 0.5
-xi = scenarios(10, timesteps)
+#xi_2 = @scenario t_xi = 3 s_xi = -1 probability = 0.5 #Int(floor((t_f+t_s)/2))
+xi = scenarios(10, t_s:t_f)
 sp = instantiate(em, xi, optimizer = GLPK.Optimizer)
 ##
 optimize!(sp)
@@ -140,6 +141,14 @@ objective_value(sp)
 ##
 plot_results(sp, pv, wind, demand, debug = true)
 
+for t in timesteps
+    xi_t = @scenario t_xi = t s_xi = -1 probability = 1.
+    sp_t = instantiate(em, [xi_t], optimizer = GLPK.Optimizer)
+    optimize!(sp_t)
+    od = optimal_decision(sp_t)
+    print(objective_value(sp_t))
+    print(", t_xi = $t \n")
+end
 ##
 # Main result
 println("Termination status: $(termination_status(sp))")
