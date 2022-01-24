7ec30045e34dc7c0154a976105dee777f1616e3c
Paul Prochaska <paul.prochaska@icloud.com>
2022-01-24 16:51:28 +0100

    heatstorage constraints

--- src/one_stage_problem.jl
+++ src/one_stage_problem.jl
@@ -23,7 +23,7 @@ end=#
 #pv = [clamped_sin(x/24. * 2. * pi) for x in timesteps]
 
 #defining timesteps 
-timesteps = 1:3
+timesteps = 1:24
 
 #pv, wind and demand data
 demand = CSV.read("../data/demand_Industriepark.csv", DataFrame)[timesteps, 1]
@@ -31,16 +31,17 @@ wind = CSV.read("../data/wind_Karholz.csv", DataFrame)[timesteps, 1]
 pv = CSV.read("../data/pv_Halle18.csv", DataFrame)[timesteps, 1]
 
 #heatsystem demand assumption
-heatdemand = [100, 100, 100]
+heatdemand = zeros(Int, 24)
+fill!(heatdemand, 100)
 
 #Global system parameter 
 c_i = .10 
 c_o = .01
 c_w = 1000.
-c_pv = 1000.
+c_pv = 2.
 c_storage = 400.
 #heatsystem
-c_heatpump = 100
+c_heatstorage = 100
 
 
 time_scale = 10. *365*24/length(timesteps)
@@ -64,20 +65,19 @@ m = Model(Cbc.Optimizer)
 @variable(m, gco[t in timesteps]>=0)
 @variable(m, storage[t in timesteps])
 #variable heatsystem
-@variable(m, 0 <= u_heatpump)
-#@variable(m, 0 <= heatstorage)
+#@variable(m, 0 <= u_heatpump)
+@variable(m, 0 <= u_heatstorage)
+@variable(m, heatstorage[t in timesteps])
 @variable(m, heatpumpflow[t in timesteps])
-@variable(m, z[t in timesteps], Bin)
+#@variable(m, z[t in timesteps], Bin)
 
 #energy balance
-@constraint(m,[t in timesteps], gci[t]-gco[t]+u_pv*pv[t]+u_w*wind[t]-demand[t]+storage[t]==0) 
+@constraint(m,[t in timesteps], gci[t]-gco[t]+u_pv*pv[t]+u_w*wind[t]-demand[t]+storage[t]-heatpumpflow[t]==0)
 #without heat storage:  -heatdemand[t]* Coefficient of Performance (COP) from heatpump
-#with heat storage: -(heatdemand[t]+heatstorage[t])
+#with heat storage: -(heatdemand[t]+heatstorage[t]) -> heatpumpflow[t]
 
 #objective function
-@objective(m, Min, c_pv/time_scale*u_pv + c_w/time_scale*u_w + u_storage*c_storage/time_scale + c_i*sum(gci) - c_o*sum(gco))
-
-
+@objective(m, Min, c_pv/time_scale*u_pv + c_w/time_scale*u_w + u_storage*c_storage/time_scale + c_i*sum(gci) - c_o*sum(gco)) + + u_heatstorage*c_heatstorage/time_scale
 
 #defining battary constraint
 #@constraint(m, [t in timesteps], -0.5*u_b*b_scale <= sum(b[1:t]))
@@ -89,6 +89,10 @@ m = Model(Cbc.Optimizer)
 @constraint(m, sum(storage) == 0)
 
 #heatstorage constraints like in battary storage
+@constraint(m, [t in timesteps], heatpumpflow[t] == heatdemand[t] + heatstorage[t])
+@constraint(m, [t in timesteps], u_heatstorage <= sum(heatstorage[1:t]))
+@constraint(m, [t in timesteps], sum(heatstorage[1:t]) <= u_heatstorage)
+@constraint(m, [t in timesteps], sum(heatstorage) == 0)
 
 #defining heatpump constraint, with binary vaiable z
 #@constraint(m, [t in timesteps], heatdemand[t] <= 1000 * (1 - z[t]) )
@@ -102,5 +106,5 @@ println("Objective value: ", getobjectivevalue(m))
 println("u_pv = ", getvalue(u_pv))
 println("u_w = ", getvalue(u_w))
 println("u_storage = ", getvalue(u_storage))
-println("u_heatpump = ", getvalue(u_heatpump))
+println("u_heatstorage = ", getvalue(u_heatstorage))
 ##
