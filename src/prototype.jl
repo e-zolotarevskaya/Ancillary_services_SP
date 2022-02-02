e4fb7631c0a95616f52c56be6e17c4a8dbd66bda
Ekaterina Zolotarevskaia <zkatya123@gmail.com>
2022-02-02 11:33:17 +0100

    Parametrize the model for easier analysis

--- src/prototype.jl
+++ src/prototype.jl
@@ -17,7 +17,7 @@ function plot_results(sp, pv, w, d; scenarios=[1], stage_1=[:gci, :gco], stage_2
     plt_sto = plot() # create separate plot for storage state of charge
     plt = plot(pv .* value(sp[1, :u_pv]), label="pv")
     plot!(plt, w .* value(sp[1, :u_wind]), label="wind")
-    plot!(plt, -d, label="demand")
+    plot!(plt, d, label="demand")
     stor_flow = value.(sp[1, :storage]).data
     stor_charge = [sum(stor_flow[1:t]) for t in value.(sp[1, :storage]).axes[1]] .+ 0.5*value(sp[1, :u_storage])
     plot!(plt_sto, value.(sp[1, :storage]).axes, stor_charge, label="global storage charge")
@@ -47,30 +47,6 @@ function plot_results(sp, pv, w, d; scenarios=[1], stage_1=[:gci, :gco], stage_2
     display(plot(plt, plt_sto, layout = (2,1)))
 end
 
-
-##
-# Global system parameters
-c_i = .03
-c_o = .01
-t_s = 1
-t_f = 48
-offset = 2400
-timesteps = t_s:t_f
-time_scale = 10. *365*24/length(timesteps)
-c_pv = 100.
-c_wind = 1000.
-c_storage = 100.
-c_flex = .5
-F = 10.
-storage_scale = 1.
-##
-# Define weather and demand data
-#=pv = CSV.read("../data/pv_Halle18.csv", DataFrame)[timesteps, 1]
-wind = CSV.read("../data/wind_Karholz.csv", DataFrame)[timesteps, 1]
-demand = CSV.read("../data/demand_Industriepark.csv", DataFrame)[timesteps, 1]=#
-pv = CSV.read("../basic_example.csv", DataFrame)[timesteps.+offset, 3]
-wind = CSV.read("../basic_example.csv", DataFrame)[timesteps.+offset, 4]
-demand = CSV.read("../basic_example.csv", DataFrame)[timesteps.+offset, 2]
 ##
 @define_scenario simple_scenario = begin
     t_xi::Int64
@@ -99,6 +75,12 @@ end
 # Note on signs: s_xi<0 means that energy is requested, s_xi>0 means an additional consumption
 energy_model = @stochastic_model begin 
     @stage 1 begin
+        @parameters begin
+            time_scale = 10. *365*24/length(timesteps)
+            c_pv = 100.
+            c_wind = 1000.
+            c_storage = 100.
+        end
         # Investments:
         @decision(model, 100000 >= u_pv >= 0)
         @decision(model, 100000 >= u_wind >= 0)
@@ -108,8 +90,8 @@ energy_model = @stochastic_model begin
         @decision(model, gco[t in timesteps] >= 0)
         # Storage model
         @decision(model, storage[t in timesteps])
-        @constraint(model, [t in timesteps], -0.5*u_storage*storage_scale <= sum(storage[t_s:t]))
-        @constraint(model, [t in timesteps], sum(storage[t_s:t]) <= 0.5*u_storage*storage_scale)
+        @constraint(model, [t in timesteps], -0.5*u_storage <= sum(storage[t_s:t]))
+        @constraint(model, [t in timesteps], sum(storage[t_s:t]) <= 0.5*u_storage)
         @constraint(model, sum(storage) == 0)
         # Energy balance
         @constraint(model, [t in timesteps], 
@@ -118,6 +100,12 @@ energy_model = @stochastic_model begin
         @objective(model, Min, (u_pv*c_pv+u_wind*c_wind+u_storage*c_storage)/time_scale)
     end
     @stage 2 begin
+        @parameters begin
+            c_i = .03
+            c_o = .01
+            #c_flex = .5
+            F = 10.
+        end
         @uncertain t_xi s_xi from simple_scenario #t_xi the time of flexibility demand, s_xi - sign (±1 or 0)
         @known(model, u_pv)
         @known(model, u_wind)
@@ -132,8 +120,8 @@ energy_model = @stochastic_model begin
         @constraint(model, [t in t_s:t_xi], gci[t] == gci2[t])
         @constraint(model, [t in t_s:t_xi], gco[t] == gco2[t])
 
-        @constraint(model, [t in timesteps], -0.5*u_storage*storage_scale <= sum(sto2[t_s:t]))
-        @constraint(model, [t in timesteps], sum(sto2[t_s:t]) <= 0.5*u_storage*storage_scale)
+        @constraint(model, [t in timesteps], -0.5*u_storage <= sum(sto2[t_s:t]))
+        @constraint(model, [t in timesteps], sum(sto2[t_s:t]) <= 0.5*u_storage)
 
         @constraint(model, sum(sto2) == 0)
 
@@ -151,8 +139,22 @@ energy_model = @stochastic_model begin
     end
 end
 ## 
-s = simple_sampler(timesteps)
+# Define the time interval
+t_s = 1
+t_f = 48
+offset = 2400
+timesteps = t_s:t_f
 
+# Define weather and demand data
+#=pv = CSV.read("../data/pv_Halle18.csv", DataFrame)[timesteps, 1]
+wind = CSV.read("../data/wind_Karholz.csv", DataFrame)[timesteps, 1]
+demand = CSV.read("../data/demand_Industriepark.csv", DataFrame)[timesteps, 1]=#
+pv = CSV.read("../basic_example.csv", DataFrame)[timesteps.+offset, 3]
+wind = CSV.read("../basic_example.csv", DataFrame)[timesteps.+offset, 4]
+demand = CSV.read("../basic_example.csv", DataFrame)[timesteps.+offset, 2]
+# 
+s = simple_sampler(timesteps)
+##
 sp = instantiate(energy_model, s, 10, optimizer = Cbc.Optimizer)
 ##
 optimize!(sp)
@@ -169,6 +171,7 @@ println("Objective value: $(objective_value(sp))")
 println("Optimal decision: $(optimal_decision(sp))")
 
 ##
+# Investment result
 u_pv = sp[1, :u_pv]
 u_wind = sp[1, :u_wind]
 u_storage = sp[1, :u_storage]
@@ -176,7 +179,6 @@ u_storage = sp[1, :u_storage]
 println("value(u_pv) = $(value(u_pv))")
 println("value(u_wind) = $(value(u_wind))")
 println("value(u_storage) = $(value(u_storage))")
-
 
 # Second stage
 for s in 1:3
