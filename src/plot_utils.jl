9b90de5f1768584a2a12a4ec67d9299c1ece0654
Ekaterina Zolotarevskaia <zkatya123@gmail.com>
2022-03-29 14:24:16 +0200

    add plot_difference

--- src/plot_utils.jl
+++ src/plot_utils.jl
@@ -39,33 +39,55 @@ end
 
 function plot_flexibility(scen_results, ov)
     T = size(scen_results)[1]
-    positive_flexibility = zeros(Int(T/2))
-    negative_flexibility = zeros(Int(T/2))
-    positive_potential = zeros(Int(T/2))
-    negative_potential = zeros(Int(T/2))
+    positive_flexibility = zeros(T ÷ 2)
+    negative_flexibility = zeros(T ÷ 2)
+    positive_potential = zeros(T ÷ 2)
+    negative_potential = zeros(T ÷ 2)
     for i in 1:T
-        if scen_results[i,3] > 0
-            if scen_results[i,1] != Inf && scen_results[i,1] != -Inf
-                positive_flexibility[Int(scen_results[i,2])] = scen_results[i,1] - ov
+        if scen_results[i, 3] > 0
+            if scen_results[i, 1] != Inf && scen_results[i, 1] != -Inf
+                positive_flexibility[Int(scen_results[i, 2])] = scen_results[i, 1] - ov
                 if scen_results != Inf
-                    positive_potential[Int(scen_results[i,2])] = scen_results[i, 3]
+                    positive_potential[Int(scen_results[i, 2])] = scen_results[i, 3]
                 end
             end
         else
-            if scen_results[i,1] != Inf && scen_results[i,1] != -Inf
-                negative_flexibility[Int(scen_results[i,2])] = scen_results[i,1] - ov
+            if scen_results[i, 1] != Inf && scen_results[i, 1] != -Inf
+                negative_flexibility[Int(scen_results[i, 2])] = scen_results[i, 1] - ov
                 if scen_results != -Inf
-                    negative_potential[Int(scen_results[i,2])] = scen_results[i, 3]
+                    negative_potential[Int(scen_results[i, 2])] = scen_results[i, 3]
                 end
             end
         end
     end
     plt_cost = plot()
     plt_pot = plot()
-    plot!(plt_cost, positive_flexibility./positive_potential, label = "price of positive flexibility")
-    plot!(plt_cost, negative_flexibility./negative_potential, label = "price of negative flexibility")
+    plot!(plt_cost, positive_flexibility ./ positive_potential, label = "price of positive flexibility")
+    plot!(plt_cost, negative_flexibility ./ negative_potential, label = "price of negative flexibility")
     plot!(plt_pot, positive_potential, fillrange = 0, fillalpha = 0.35, label = "positive flexibility potential")
     plot!(plt_pot, negative_potential, fillrange = 0, fillalpha = 0.35, label = "negative flexibility potential")
-    display(plot(plt_cost, plt_pot, layout = (2,1)))
+    display(plot(plt_cost, plt_pot, layout = (2, 1)))
     return positive_flexibility, negative_flexibility, positive_potential, negative_potential
+en
+
+function plot_difference(sp; s = 1)
+    plt_gc = plot()
+    plt_sto = plot()
+    t_xi = scenarios(sp)[s].data.t_xi
+    recovery_time = sp.stages[2].parameters[:recovery_time]
+
+    plot!(plt_gc, -value.(sp[1, :gci])[t_xi+1:t_xi+recovery_time]+value.(sp[2, :gci2], s), xlabel = "time after the event, h", label = "gci2-gci")
+    plot!(plt_gc, -value.(sp[1, :gco])[t_xi+1:t_xi+recovery_time]+value.(sp[2, :gco2], s), xlabel = "time after the event, h", label = "gco2-gco")
+    
+    stor_flow_i = value.(sp[1, :sto_in])[t_xi:t_xi+recovery_time]
+    stor_flow_o = value.(sp[1, :sto_out])[t_xi:t_xi+recovery_time]
+    stor_charge = [-sum(stor_flow_i[1:t]) + sum(stor_flow_o[1:t]) for t in 1:recovery_time+1] .+ 0.5 * value(sp[1, :u_storage])
+    stor_flow_i2 = value.(sp[2, :sto_in2], s)
+    stor_flow_o2 = value.(sp[2, :sto_out2], s)
+    stor_charge2 = [-sum(stor_flow_i2[1:t]) + sum(stor_flow_o2[1:t]) for t in 1:(recovery_time+1)] .+ stor_charge[t_xi-1]
+    
+    plot!(plt_sto, stor_charge2-stor_charge, label = "soc2-soc")
+
+
+    display(plot(plt_gc, plt_sto, layout = (2, 1)))
 end
